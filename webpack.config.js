6fd2f0167f2caa7b57ecfe68e99e6e288b647e18
Alexander <alexandertisko@hotmail.com>
2020-10-11 16:04:44 -0300

    orange

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+
 module.exports = {
-    entry: './app/assets/scripts/App.js'
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js', 
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    }
 }
